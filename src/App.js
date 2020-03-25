67803ce81c0d580656fba27e5b4968c3caf0d163
Meagle <mindfulyeon@gmail.com>
2020-03-25 15:47:50 +0900

    carousel 라이브러리 추가, 스타일 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import "./App.css";
 import BodyPart from "./components/BodyPart";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 const data = [
   { title: "Neck & Shoulder", backgroundColor: "#94ccfe", backgroundImage: "" },
@@ -19,14 +21,62 @@ function App() {
         <h1 className="title yogacamp">Yoga Camp</h1>
       </div>
       <div className="body-align">
-        {data.map((item, index) => (
-          <BodyPart
-            key={index}
-            title={item.title}
-            backgroundColor={item.backgroundColor}
-          />
-        ))}
-
+        <Carousel
+          additionalTransfrom={0}
+          arrows={false}
+          autoPlaySpeed={3000}
+          centerMode
+          className=""
+          containerClass="container"
+          dotListClass=""
+          draggable
+          focusOnSelect={false}
+          infinite={false}
+          itemClass="carousel-item"
+          keyBoardControl
+          minimumTouchDrag={80}
+          renderButtonGroupOutside={false}
+          renderDotsOutside={false}
+          responsive={{
+            desktop: {
+              breakpoint: {
+                max: 3000,
+                min: 1024
+              },
+              items: 3,
+              partialVisibilityGutter: 40
+            },
+            mobile: {
+              breakpoint: {
+                max: 464,
+                min: 0
+              },
+              items: 1,
+              partialVisibilityGutter: 30
+            },
+            tablet: {
+              breakpoint: {
+                max: 1024,
+                min: 464
+              },
+              items: 2,
+              partialVisibilityGutter: 30
+            }
+          }}
+          showDots={false}
+          sliderClass=""
+          slidesToSlide={1}
+          swipeable
+        >
+          {data.map((item, index) => (
+            <BodyPart
+              key={index}
+              title={item.title}
+              backgroundColor={item.backgroundColor}
+            />
+          ))}
+        </Carousel>
+        ;
         {/* <BodyPart title={"Neck"} />
         <BodyPart title={"Shoulder"} />
         <BodyPart />
