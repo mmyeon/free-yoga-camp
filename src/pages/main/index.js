506776143559ae7c4a77b64b99e4f94de7e02fd5
Meagle <mindfulyeon@gmail.com>
2020-03-30 17:13:21 +0900

    [리팩토링] 컴포넌트 분리 > main컴포넌트 분리

---
+++ src/pages/main/index.js
@@ -0,0 +1,92 @@
+import React from "react";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import db from "../../db";
+import { HashRouter as Router, Route, Link } from "react-router-dom";
+import Neck from "../../routes/Neck";
+import "./index.css";
+import BodyPart from "../../components/BodyPart";
+
+export default function Main() {
+  return (
+    <div className="App">
+      <div className="title_display">
+        <h1 className="title free">Free</h1>
+        <h1 className="title yogacamp">Yoga Camp</h1>
+      </div>
+      <div className="body-align">
+        <Carousel
+          additionalTransfrom={0}
+          arrows={false}
+          autoPlaySpeed={3000}
+          centerMode
+          className=""
+          containerClass="container"
+          dotListClass=""
+          draggable
+          focusOnSelect={false}
+          infinite={false}
+          itemClass="carousel-item"
+          keyBoardControl
+          minimumTouchDrag={80}
+          renderButtonGroupOutside={false}
+          renderDotsOutside={false}
+          responsive={{
+            desktop: {
+              breakpoint: {
+                max: 3000,
+                min: 1024
+              },
+              items: 3,
+              partialVisibilityGutter: 40
+            },
+            mobile: {
+              breakpoint: {
+                max: 464,
+                min: 0
+              },
+              items: 1,
+              partialVisibilityGutter: 30
+            },
+            tablet: {
+              breakpoint: {
+                max: 1024,
+                min: 464
+              },
+              items: 2,
+              partialVisibilityGutter: 30
+            }
+          }}
+          showDots={false}
+          sliderClass=""
+          slidesToSlide={1}
+          swipeable
+        >
+          <Router>
+            <Route path="/neck" exact={true} component={Neck} />
+            <Route path="/lowerbac" exact={true} component={Neck} />
+
+            {db.map(item => (
+              <Link to="neck">
+                <BodyPart
+                  key={item.id}
+                  title={item.title}
+                  backgroundColor={item.backgroundColor}
+                />
+              </Link>
+            ))}
+          </Router>
+        </Carousel>
+
+        {/* <BodyPart title={"Neck"} />
+        <BodyPart title={"Shoulder"} />
+        <BodyPart />
+        <BodyPart />
+        <BodyPart />
+        <BodyPart />
+        <BodyPart />
+        <BodyPart /> */}
+      </div>
+    </div>
+  );
+}
