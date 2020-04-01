1b9e0c05756ab6d5c92ab6a639caacf34c1092e0
Meagle <mindfulyeon@gmail.com>
2020-04-01 17:09:10 +0900

    프로젝트 리셋

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,53 +1,7 @@
 import React from "react";
-import db from "../db";
-import "./Detail.css";
-import Videolist from "./Videolist";
 
-function Detail(props) {
-  // console.log(props.match.params.id);
-  // console.log(db[0].desc);
-  const rightBodyPart = db.find(item => item.id === props.match.params.id);
-  // const yogaVideoList = db.map(item => )
-
-  return (
-    <>
-      <h1 className="detail-desc">{rightBodyPart.desc}</h1>
-      <ul>
-        <li>
-          <div className="circle">
-            <svg height="100px" width="100px">
-              <g>
-                <title>Not Passed</title>
-                <circle
-                  cx="50px"
-                  cy="50px"
-                  r="10px"
-                  stroke="black"
-                  strokeWidth="3px"
-                  fill="white"
-                ></circle>
-              </g>
-            </svg>
-            <div className="yogaVideoList">
-              <ul>
-                {rightBodyPart.playList.map((item, index) => {
-                  return (
-                    <li key={index}>
-                      <a href={item.url}>{item.title}</a>
-                      <Videolist />
-                    </li>
-                  );
-                })}
-              </ul>
-            </div>
-            <a href="https://developer.mozilla.org/ko/docs/Web/SVG/Element/circle">
-              하하하
-            </a>
-          </div>
-        </li>
-      </ul>
-    </>
-  );
-}
+const Detail = () => {
+  return <div></div>;
+};
 
 export default Detail;
