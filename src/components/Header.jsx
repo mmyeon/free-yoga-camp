0ca6ddb24916797def58e14faea0e5e0c52e7395
Meagle <mindfulyeon@gmail.com>
2020-04-02 15:23:07 +0900

    헤더 > 네비게이션 바, 아이콘 정렬 및 디자인 적용

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import "./Header.scss";
 
-const Navigation = () => {
+const Header = () => {
   return (
     <div className="header">
-      <div className="header container">
-        <i className="fas fa-bars header-icon nav-bars"></i>
-        <i className="fas fa-wind header-icon fcc-logo"></i>
+      <div className="container">
+        <a href="dd">
+          <i className="fas fa-bars nav-bars"></i>
+        </a>
+        <a href="dd">
+          <i className="fas fa-wind logo"></i>
+        </a>
       </div>
     </div>
   );
 };
 
-export default Navigation;
+export default Header;
