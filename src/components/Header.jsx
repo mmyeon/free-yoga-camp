e2d70cc6f70f81f08b69c2e89ff32260f357f712
Meagle <mindfulyeon@gmail.com>
2020-04-02 12:47:14 +0900

    헤더 스크롤 시 상단 위치하도록 디자인 적용

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,9 +4,9 @@ import "./Header.scss";
 const Navigation = () => {
   return (
     <div className="header">
-      <div className="container">
-        <i className="fas fa-bars nav-bars"></i>
-        <i className="fas fa-wind nav-logo"></i>
+      <div className="header container">
+        <i className="fas fa-bars header-icon nav-bars"></i>
+        <i className="fas fa-wind header-icon fcc-logo"></i>
       </div>
     </div>
   );
