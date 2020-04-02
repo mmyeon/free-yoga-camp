3f10fb186f3b07cae4dd7266a150991a57a7c588
Meagle <mindfulyeon@gmail.com>
2020-04-02 16:37:29 +0900

    사이드 내비게이션바 만들기, 리액트 훅 사용

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.scss";
 
 const Header = () => {
+  const [isOpenedSideNav, setIsOpenedSideNav] = useState(false);
+
+  function handleClick() {
+    setIsOpenedSideNav(!isOpenedSideNav);
+    // console.log("ghcnf");
+  }
+
   return (
-    <div className="header">
-      <div className="container">
-        <a href="dd">
-          <i className="fas fa-bars nav-bars"></i>
-        </a>
-        <a href="dd">
-          <i className="fas fa-wind logo"></i>
-        </a>
+    <div>
+      {isOpenedSideNav && (
+        <div id="sideNav">
+          <ul>
+            <li>상체를 위한 요가</li>
+          </ul>
+          <div onClick={handleClick}>닫기</div>
+        </div>
+      )}
+      <div className="header">
+        <div className="container">
+          <a onClick={handleClick}>
+            <i className="fas fa-bars nav-bars"></i>
+          </a>
+          <a href="dd">
+            <i className="fas fa-wind logo"></i>
+          </a>
+        </div>
       </div>
     </div>
   );
