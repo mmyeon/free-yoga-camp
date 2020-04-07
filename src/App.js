113835a7bedac902dfd4e485eeb8f61ff2f119a5
Meagle <mindfulyeon@gmail.com>
2020-04-07 16:36:47 +0900

    컴포넌트 이름 수정 Home -> Main

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { HashRouter, Route } from "react-router-dom";
 import Header from "./components/Header";
-import Home from "./components/Home";
+import Main from "./components/Main";
 import Detail from "./routes/Detail";
 
 import "./App.scss";
@@ -11,7 +11,7 @@ function App() {
     <HashRouter>
       <Header />
       <div style={{ marginTop: "50px" }}>
-        <Route path="/" exact={true} component={Home} />
+        <Route path="/" exact={true} component={Main} />
         <Route path="/body/:id" component={Detail} />
       </div>
     </HashRouter>
