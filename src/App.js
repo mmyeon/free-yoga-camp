e6c478f2f4889579b7fddf25400659b5bb9c065f
Meagle <mindfulyeon@gmail.com>
2020-04-10 14:41:36 +0900

    Route 설정

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { HashRouter, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Detail from "./routes/Detail";
 
 import "./App.scss";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <HashRouter>
+        <Header />
+        <Route path="/" exact={true} component={Main}></Route>
+        <Route path="/id" component={Detail}></Route>
+      </HashRouter>
+    </div>
+  );
 }
 
 export default App;
