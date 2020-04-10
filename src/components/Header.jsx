f4cff67de9e50fe872dd8e9edfc1c667b2494c52
Meagle <mindfulyeon@gmail.com>
2020-04-10 16:50:32 +0900

    상단 헤더 만들기

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,27 @@ import React from "react";
 import "./Header.scss";
 
 const Header = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="top-header">
+        <div className="top-header icons">
+          <i className="fas fa-bars nav-bars"></i>
+          <i className="fas fa-wind logo"></i>
+        </div>
+      </div>
+      {/* <div className="side-nav container">
+        <div className="side-nav">
+          <ul>
+            <li>상체를 위한 요가</li>
+            <li>상체를 위한 요가</li>
+            <li>상체를 위한 요가</li>
+            <li>상체를 위한 요가</li>
+          </ul>
+        </div>
+        <div className="side-nav overlay"></div>
+      </div> */}
+    </div>
+  );
 };
 
 export default Header;
