756339ecf5ea1aff3674bfc6f5e7a61248d7862e
Meagle <mindfulyeon@gmail.com>
2020-04-10 17:12:47 +0900

    클릭시 sidenav 열리기 - 리액트 hook 사용

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.scss";
 
 const Header = () => {
+  const [isOpenedSidenav, setIsOpenedSidenav] = useState(false);
+
+  function handleClick() {
+    setIsOpenedSidenav(!isOpenedSidenav);
+  }
+
   return (
     <div>
       <div className="top-header">
         <div className="top-header icons">
-          <i className="fas fa-bars nav-bars"></i>
+          <i className="fas fa-bars nav-bars" onClick={handleClick}></i>
           <i className="fas fa-wind logo"></i>
         </div>
       </div>
-      {/* <div className="side-nav container">
-        <div className="side-nav">
-          <ul>
-            <li>상체를 위한 요가</li>
-            <li>상체를 위한 요가</li>
-            <li>상체를 위한 요가</li>
-            <li>상체를 위한 요가</li>
-          </ul>
-        </div>
-        <div className="side-nav overlay"></div>
-      </div> */}
+      <div className="sidenav-container">
+        {isOpenedSidenav && (
+          <div className="sidenav">
+            <ul>
+              <li>상체를 위한 요가</li>
+              <li>상체를 위한 요가</li>
+              <li>상체를 위한 요가</li>
+              <li>상체를 위한 요가</li>
+            </ul>
+          </div>
+        )}
+        <div className="sidenav overlay"></div>
+      </div>
     </div>
   );
 };
