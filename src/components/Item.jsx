24e9fd88f00f78f289348d8e406c6d12f304268d
Meagle <mindfulyeon@gmail.com>
2020-04-12 18:27:43 +0900

    요가  이미지 클릭시 이미지 커지는 기능 추가

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,8 +1,37 @@
 import React from "react";
 import "./Item.scss";
 
-const Item = () => {
-  return <div></div>;
+const Item = ({
+  id,
+  title,
+  desc,
+  bgColor,
+  activeItemId,
+  setActiveItemId,
+  imageUrl,
+  objectPosition,
+}) => {
+  function handleClick() {
+    setActiveItemId({ id });
+  }
+  return (
+    <div
+      className={`body-item ${activeItemId.id === id ? "clicked" : ""}`}
+      onClick={handleClick}
+    >
+      <img
+        className={`yoga-pose ${activeItemId.id === id ? "clicked" : ""}`}
+        src={imageUrl}
+        alt="yoga pose"
+        width="100%"
+        height="100%"
+        style={{ objectPosition }}
+      />
+      {/* <div className="text">
+        <div className="title">{title}</div>
+      </div> */}
+    </div>
+  );
 };
 
 export default Item;
