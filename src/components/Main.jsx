e2864e08f3d4636e28562ef1f5f11f91c7f29051
Meagle <mindfulyeon@gmail.com>
2020-04-12 17:21:52 +0900

    아이템 컴포넌트를 관리할 메인 컴포넌트 제작 및 스타일 적용 - 첫 번째와 마지막 아이템에 border radius 적용

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import db from "../db";
+import Item from "./Item";
 import "./Main.scss";
 
 const Main = () => {
-  return <div></div>;
+  const [activeItemId, setActiveItemId] = useState("");
+
+  return (
+    <div className="item-container">
+      {db.map((item) => (
+        <Item
+          id={item.id}
+          key={item.id}
+          title={item.title}
+          desc={item.desc}
+          bgColor={item.backgroundColor}
+          imageUrl={item.imageUrl}
+          activeItemId={activeItemId}
+          setActiveItemId={setActiveItemId}
+          objectPosition={item.objectPosition}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default Main;
