13dc801fce39348270756499ca60f21ba0c23c3f
Meagle <mindfulyeon@gmail.com>
2020-04-12 16:54:27 +0900

    헤더 컴포넌트 백업 및 개선 - a 태그 삭제 - 헤더 아이콘 중앙정렬

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,32 +2,40 @@ import React, { useState } from "react";
 import "./Header.scss";
 
 const Header = () => {
-  const [isOpenedSidenav, setIsOpenedSidenav] = useState(false);
+  const [isOpenedSideNav, setIsOpenedSideNav] = useState(false);
 
   function handleClick() {
-    setIsOpenedSidenav(!isOpenedSidenav);
+    setIsOpenedSideNav(!isOpenedSideNav);
+    // console.log("ghcnf");
   }
 
   return (
     <div>
-      <div className="top-header">
-        <div className="top-header icons">
-          <i className="fas fa-bars nav-bars" onClick={handleClick}></i>
-          <i className="fas fa-wind logo"></i>
-        </div>
-      </div>
-      <div className="sidenav-container">
-        {isOpenedSidenav && (
-          <div className="sidenav">
+      <div
+        className={`side-nav-overlay ${
+          isOpenedSideNav ? "overlay-active" : ""
+        }`}
+      >
+        <div className={`side-nav ${isOpenedSideNav ? "active" : ""}`}>
+          <i className="fas fa-times close-btn" onClick={handleClick}></i>
+          {isOpenedSideNav && (
             <ul>
               <li>상체를 위한 요가</li>
               <li>상체를 위한 요가</li>
               <li>상체를 위한 요가</li>
               <li>상체를 위한 요가</li>
             </ul>
+          )}
+        </div>
+      </div>
+      {/* // </div> */}
+      <div className="header">
+        <div className="container">
+          <div className="header-icons">
+            <i className="fas fa-bars nav-bars" onClick={handleClick}></i>
+            <i className="fas fa-wind logo"></i>
           </div>
-        )}
-        <div className="sidenav overlay"></div>
+        </div>
       </div>
     </div>
   );
