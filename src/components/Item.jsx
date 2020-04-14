0ec04cfaf7dd7ae796dfb51a6f60581c2cfc0166
Meagle <mindfulyeon@gmail.com>
2020-04-15 00:29:40 +0900

    버튼 클릭시 상세페이지 이동하도록 Link

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Item.scss";
+import { Link } from "react-router-dom";
 
 const Item = ({
   id,
@@ -30,12 +31,22 @@ const Item = ({
         <div className={`title-detail ${activeItemId === id ? "visible" : ""}`}>
           {titleDetail}
         </div>
-        <div className={`action-text ${activeItemId === id ? "visible" : ""}`}>
-          수련하기
-        </div>
+        <Link to={{ pathname: `/${id}` }}>
+          <div
+            className={`action-text ${activeItemId === id ? "visible" : ""}`}
+          >
+            수련하기
+          </div>
+        </Link>
       </div>
     </div>
   );
+
+  // <Link
+  //   to={{
+  //     pathname: `/${item.id}`,
+  //   }}
+  // >
 
   function handleClick() {
     setActiveItemId(id);
