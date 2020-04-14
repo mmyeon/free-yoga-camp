e629f2d245c477cb67b3a2d7b75765ba3e9e2fdf
Meagle <mindfulyeon@gmail.com>
2020-04-15 00:20:41 +0900

    타이틀 설명해주는 titleDetail 프롭 추가

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import db from "../db";
 import Item from "./Item";
+// import { Link } from "react-router-dom";
 import "./Main.scss";
 
 const Main = () => {
@@ -9,10 +10,16 @@ const Main = () => {
   return (
     <div className="item-container">
       {db.map((item) => (
+        // <Link
+        //   to={{
+        //     pathname: `/${item.id}`,
+        //   }}
+        // >
         <Item
           id={item.id}
           key={item.id}
           title={item.title}
+          titleDetail={item.titleDetail}
           desc={item.desc}
           bgColor={item.backgroundColor}
           imageUrl={item.imageUrl}
@@ -20,6 +27,7 @@ const Main = () => {
           setActiveItemId={setActiveItemId}
           objectPosition={item.objectPosition}
         />
+        // </Link>
       ))}
     </div>
   );
