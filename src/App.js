f7f877fb2b25c5780e7c0fcb72863912f7f75a17
Meagle <mindfulyeon@gmail.com>
2020-04-16 12:36:18 +0900

    VideoList 컴포넌트 연결하기

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { HashRouter, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Main from "./components/Main";
-import Detail from "./routes/Detail";
+import VideoList from "./routes/VideoList";
 
 import "./App.scss";
 
@@ -13,7 +13,7 @@ function App() {
         <Header />
         <Route path="/" exact={true} component={Main}></Route>
         {/* <Route path="/id" component={Detail}></Route> */}
-        <Route path="/:id" component={Detail}></Route>
+        <Route path="/:id" component={VideoList}></Route>
       </HashRouter>
     </div>
   );
