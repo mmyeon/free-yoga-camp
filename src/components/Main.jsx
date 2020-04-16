ab5b5528b3158413ad3af0f528ffcc3d5cab0904
Meagle <mindfulyeon@gmail.com>
2020-04-16 12:30:54 +0900

    메인페이지 코드 정리 - Link 삭제

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import db from "../db";
 import Item from "./Item";
-// import { Link } from "react-router-dom";
 import "./Main.scss";
 
 const Main = () => {
@@ -10,11 +9,6 @@ const Main = () => {
   return (
     <div className="item-container">
       {db.map((item) => (
-        // <Link
-        //   to={{
-        //     pathname: `/${item.id}`,
-        //   }}
-        // >
         <Item
           id={item.id}
           key={item.id}
